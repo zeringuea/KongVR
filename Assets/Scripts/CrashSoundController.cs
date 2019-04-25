76e8a5a300d1553f146d3af4fef27b0063b4300a
hyperslab <silasjm6@gmail.com>
2019-04-25 14:22:08 -0400

    Polish stuff

---
+++ Assets/Scripts/CrashSoundController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashSoundController : MonoBehaviour
+{
+
+    public AudioClip crashNoise;
+    public float speedThreshold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = crashNoise;
+    }
+    
+    void OnCollisionEnter()
+    {
+        Debug.Log(GetComponent<Rigidbody>().velocity.magnitude);
+        if (GetComponent<Rigidbody>().velocity.magnitude >= speedThreshold)
+        {
+            Debug.Log("Crash!");
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Assets/Scripts/SceneChangeOnCollide.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChangeOnCollide : MonoBehaviour
+{
+    public string scene = "KongCity";
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.GetComponent<VRTK.VRTK_PlayerObject>() != null)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+        }
+    }
+}
